2115e4e6ba4fc254ae41663ae6ac2e5b92543563
diaztatiana <diaz.k.tatiana@gmail.com>
2020-05-26 17:46:15 +0200

    added test coverage

--- src/calculette.js
+++ src/calculette.js
@@ -3,5 +3,9 @@ export class Calculette {
     additionne (operande1, operande2) {
         return operande1 + operande2;
     }
+    soustrait (operande1, operande2) {
+        return operande1 - operande2;
+    }
 
 }
+
